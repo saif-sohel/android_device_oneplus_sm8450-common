735f25bb699f6d7c6fde728a0fad9e3c2ccb5f94
saif-sohel <saifulislamsohel30@gmail.com>
2025-02-20 06:29:08 +0000

    introduce exp.lhdc

--- common.mk
+++ common.mk
@@ -581,5 +581,13 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
+
+# LHDC
+PRODUCT_PACKAGES += \
+    liblhdc \
+    liblhdcBT_enc \
+    liblhdcv5 \
+    liblhdcv5BT_enc    
+
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8450-common/sm8450-common-vendor.mk)
