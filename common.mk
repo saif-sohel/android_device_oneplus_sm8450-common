33735fc52ab52c1619755fe1c1ebb9a8d42f8bc6
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    sm8350-common: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567

--- common.mk
+++ common.mk
@@ -33,11 +33,6 @@ PRODUCT_PACKAGES += \
     libgui_shim \
     libhidlbase_shim
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant@1.0.vendor
-
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
