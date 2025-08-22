eb41516a9f00411af601382110b090df210b43a7
saif-sohel <saifulislamsohel30@gmail.com>
2025-08-22 13:46:31 +0000

    sm8450-common: move to a new kernel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,7 +104,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8450
-TARGET_KERNEL_CONFIG := gki_defconfig vendor/waipio_GKI.config vendor/udon.config vendor/oplus_GKI.config vendor/debugfs.config
+TARGET_KERNEL_CONFIG := gki_defconfig vendor/waipio_GKI.config vendor/oplus_GKI.config vendor/debugfs.config
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(TARGET_KERNEL_SOURCE)/modules.vendor_blocklist.msm.waipio
